73468ad817f4f683b464b3bcf369f237236bfeea
Jody Bailey <jbailey@2u.com>
2024-10-09 14:35:38 +0200

    multiple args bug fix

--- project.py
+++ project.py
@@ -88,9 +88,12 @@ def handle_recommend(db, args):
 
 
 def handle_watch_list(db, args):
+    # The first arg of args._get_kwargs() is the argument's name and the second is the value of the argument
+    argument_count = list(filter(lambda argument: argument[1] not in [False, None], args._get_kwargs()))
+    
     if args.genres or args.formats or args.status or args.min_score or args.max_episodes:
         return print(INVALID_WATCH_LIST_FLAG_MSG)
-    elif len(args._get_kwargs()) > 2:
+    elif argument_count > 2:
         return print(MULTIPLE_FLAGS_ERR_MSG)
 
     if args.list:
